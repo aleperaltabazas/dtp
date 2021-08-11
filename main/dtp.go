33bc2ed6f9abd30e29c6ee46c807c87d871f3144
Alejandro <aleperaltabazas@hotmail.com>
2021-08-10 23:46:14 -0300

    add config command, with nano, should add option to use a different editor (gedit, vi, etc)

--- main/dtp.go
+++ main/dtp.go
@@ -5,15 +5,38 @@ import (
 	"github.com/aleperaltabazas/dtp/console"
 	"github.com/aleperaltabazas/dtp/global"
 	"github.com/aleperaltabazas/dtp/handlers"
+	"log"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	args := os.Args
 
+	if len(args) < 2 {
+		fmt.Println("Missing: COMMAND\n\nUsage: dtp COMMAND")
+		os.Exit(1)
+	}
+
 	switch args[1] {
 	case "config":
 		configCmd.Parse(args[2:])
+		home, err := os.UserHomeDir()
+
+		if err != nil {
+			log.Fatalf("There was an error getting the user home: %v", err)
+		}
+
+		cmd := exec.Command("/bin/nano", fmt.Sprintf("%s/.config/dtp.yaml", home))
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+
+		if err != nil {
+			fmt.Printf("Failed to open editor: %v\n", err)
+			os.Exit(1)
+		}
 	case "start":
 		handlers.Init()
 		askId()
@@ -26,7 +49,7 @@ func main() {
 
 		println("Bye!")
 	default:
-		fmt.Println("Missing: COMMAND\n\nUsage: dtp COMMAND")
+		fmt.Printf("Invalid argument '%s'\n\nUsage: dtp COMMAND\n", args[1])
 		os.Exit(1)
 	}
 
